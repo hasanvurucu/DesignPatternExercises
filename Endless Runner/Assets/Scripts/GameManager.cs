b3d3191ae421fbba4626bf83533fd9a3e71d1c59
hasanvurucu <47665574+hasanvurucu@users.noreply.github.com>
2020-09-05 19:47:23 +0300

    The very first commit
    This commit includes a simple endless runner game which I created to exercise on singleton pattern. The game is not done yet.

---
+++ Endless Runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameManager _instance;
+    public static GameManager Instance => _instance;
+    
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    [SerializeField]
+    private int score;
+
+    private float pointSpawnTimer;
+    [SerializeField]
+    private Transform spawnPoint1, spawnPoint2;
+
+    private void Start()
+    {
+        score = 0;
+        pointSpawnTimer = 0f;
+    }
+
+    private void Update()
+    {
+        PointSpawnCountdown();
+    }
+
+    public void IncreaseScore(int score)
+    {
+        this.score += score;
+    }
+
+    private void PointSpawnCountdown()
+    {
+        pointSpawnTimer += Time.deltaTime;
+        if(pointSpawnTimer >= 2f)
+        {
+            //spawn new point at spawnpoint 1 or 2
+            pointSpawnTimer = 0f;
+        }
+    }
+}
---
+++ Endless Runner/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == Tags.POINT_TAG)
+        {
+            GameManager.Instance.IncreaseScore(1);
+        }
+    }
+}
